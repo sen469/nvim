869c2d18952192f66a375c8d06708d0d5e4ce360
ssawa <sw20050719@gmail.com>
2026-03-14 17:52:10 +0900

    add smear-cursor.nvim

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,14 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    stiffness = 0.55,
+    trailing_stiffness = 0.3,
+    damping = 1.0,
+    smear_insert_mode = true,
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    scroll_buffer_space = true,
+    distance_stop_animating = 0.2,
+	cursor_coror = "#ffffff",
+  }
+}
