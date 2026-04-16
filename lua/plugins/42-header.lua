bd4d27a310ab231ab181c6a579a3811552cbebb8
ssawa <sw20050719@gmail.com>
2026-04-16 09:52:47 +0900

    42headerのメアドを変更

--- lua/plugins/42-header.lua
+++ lua/plugins/42-header.lua
@@ -4,11 +4,12 @@ return
   cmd = { "Stdheader" },
   keys = { "<F1>" },
   opts = {
-    default_map = true, -- Default mapping <F1> in normal mode.
-    auto_update = true, -- Update header when saving.
-    user = os.getenv("USER") or "user", -- Your user.
-    mail = (os.getenv("USER") or "user") .. "@student.42tokyo.jp", -- Your mail.
-    -- add other options.
+    default_map = true,
+    auto_update = true,
+    -- user = os.getenv("USER") or "user",
+    -- mail = (os.getenv("USER") or "user") .. "@student.42tokyo.jp",
+	user = "ssawa",
+	mail = "ssawa@student.42tokyo.jp",
   },
   config = function(_, opts)
     require("42header").setup(opts)
