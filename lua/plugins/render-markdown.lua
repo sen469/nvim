0554b5c37fca74cd73b8a818515d1fc58da0c6df
ssawa <sw20050719@gmail.com>
2026-04-19 13:44:54 +0900

    add render-markdown

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+  ft = { 'markdown', 'quarto' },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    file_types = { 'markdown', 'quarto' },
+  },
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "javascript", "html", "cpp" },
+            ensure_installed = { "c", "lua", "vim", "javascript", "html", "cpp", "markdown", "markdown_inline" },
             sync_install = false,
             highlight = {
                 enable = true,
