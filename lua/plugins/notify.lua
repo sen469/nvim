e1c5bfe76980517d715bf7dfc6cbc509156553f0
ssawa <sw20050719@gmail.com>
2026-05-03 10:08:26 +0900

    通知ウィンドウを右下に変更

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -6,6 +6,7 @@ return {
     render = "compact", -- よりコンパクトな表示形式を使用
     stages = "fade",    -- フェードアニメーション
     timeout = 3000,     -- 表示時間を3秒に短縮
+    top_down = false,   -- 下から上に積み上げる
   },
   config = function(_, opts)
     require("notify").setup(opts)
