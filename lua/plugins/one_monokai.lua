950d09e9c7ab9a34e47951c705f2dfd97db1f3b2
ssawa <sw20050719@gmail.com>
2026-05-14 22:33:06 +0900

    fix: one_monokai

--- lua/plugins/one_monokai.lua
+++ lua/plugins/one_monokai.lua
@@ -90,10 +90,11 @@ return {
       vim.api.nvim_set_hl(0, "@keyword.return",    { fg = colors.orange_red, italic = false })
       vim.api.nvim_set_hl(0, "@preproc",           { fg = colors.orange_red, italic = false })
       vim.api.nvim_set_hl(0, "@include",           { fg = colors.orange_red, italic = false })
-      vim.api.nvim_set_hl(0, "@define",            { fg = colors.orange_red, italic = false })
+      vim.api.nvim_set_hl(0, "@keyword.directive.define",            { fg = colors.orange_red, italic = false }) -- NOTE:
       vim.api.nvim_set_hl(0, "@repeat",            { fg = colors.orange_red, italic = false })
       vim.api.nvim_set_hl(0, "@conditional",       { fg = colors.orange_red, italic = false })
       vim.api.nvim_set_hl(0, "@keyword.typedef",   { fg = colors.orange_red, italic = false })
+      vim.api.nvim_set_hl(0, "@keyword.directive",   { fg = colors.orange_red, italic = false })
 
       -- 2. 数字 (Purple)
       vim.api.nvim_set_hl(0, "@number",            { fg = colors.purple, italic = false })
@@ -102,6 +103,8 @@ return {
       vim.api.nvim_set_hl(0, "@type.builtin",      { fg = colors.cyan, italic = false })
       vim.api.nvim_set_hl(0, "@function",          { fg = colors.cyan, italic = false })
       vim.api.nvim_set_hl(0, "@structure",         { fg = colors.cyan, italic = false })
+      vim.api.nvim_set_hl(0, "@keyword.type",           { fg = colors.cyan, italic = false }) -- NOTE:
+      vim.api.nvim_set_hl(0, "@constant.builtin",      { fg = colors.cyan, italic = false })
 
       -- 4. ユーザー定義型, 組み込み変数 (Blue)
       vim.api.nvim_set_hl(0, "@type",              { fg = colors.blue, italic = false })
@@ -111,6 +114,11 @@ return {
       vim.api.nvim_set_hl(0, "@variable",               { fg = colors.white, italic = false })
       vim.api.nvim_set_hl(0, "@punctuation.delimiter",  { fg = colors.white, italic = false })
       vim.api.nvim_set_hl(0, "@namespace",              { fg = colors.white, italic = false })
+      vim.api.nvim_set_hl(0, "@module",              { fg = colors.white, italic = false })
+
+      -- 6. 関数 (Green)
+      vim.api.nvim_set_hl(0, "@function",              { fg = colors.green, italic = false })
+
 
 
       -- -----------------------------------------------------------------------
