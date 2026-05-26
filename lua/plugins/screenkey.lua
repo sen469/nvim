2ff4be8c4505f572a713c821d26b32069317e5e9
ssawa <sw20050719@gmail.com>
2026-05-26 18:05:02 +0900

    右上表示に変更

--- lua/plugins/screenkey.lua
+++ lua/plugins/screenkey.lua
@@ -5,6 +5,9 @@ return {
   opts = {
     win_opts = {
       relative = "editor",
+      anchor = "NE",
+      row = 1,
+      col = vim.o.columns - 1,
       border = "rounded",
       style = "minimal",
       focusable = false,
