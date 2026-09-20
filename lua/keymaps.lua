645ce6a596118c590c069e379ec0c2663cb51bbb
sen469 <sw20050719@gmail.com>
2026-09-20 11:09:38 +0900

    上書きペーストコマンドpalを追加

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -61,6 +61,7 @@ end, make_opts("Toggle Neo-tree"))
 -- 編集系ユーティリティ
 vim.keymap.set("n", "cpal", ":%y<CR>", make_opts("Copy All Text"))
 vim.keymap.set("n", "clr",  ":%d<CR>", make_opts("Clear All Text"))
+vim.keymap.set("n", "pal", "ggVGp<CR>", make_opts("Paste All Text"))
 
 -- =========================================================
 -- バッファ操作ロジック
